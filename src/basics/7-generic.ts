69d0a2acf7065a277544fa7780b23d755f3066e7
VladVes <veselov.workflow@gmail.com>
2020-03-13 18:58:59 +0700

    update

--- src/basics/7-generic.ts
+++ src/basics/7-generic.ts
@@ -5,5 +5,26 @@ function reverse<T>(array: T[]): T[] {
   return array.reverse();
 }
 
-reverse(arrOfNumbers);
+reverse<number>(arrOfNumbers);
 reverse(arrOfStrings); // same func works fine with the arrays of different types
+
+
+function f<T>(a: T) {
+  return a;
+}
+
+f<string>('zzz');
+// the same as
+f('zzz'); // type will be calculated automatically
+
+type Gen<T = string> = {
+  a: string;
+  b: number;
+  c: T;
+};
+
+function f2<T>(a: T): T {
+  return a;
+}
+
+f2<Gen<boolean>>({ a: 'zzz', b: 1, c: true });
--- src/decorators.ts
+++ src/decorators.ts
@@ -1,3 +1,15 @@
+
+const rect31: RectWithArea = {
+  id: '444sdf',
+  size: {
+    width: 42,
+    height: 22,
+  },
+  getArea(): number {
+    return this.size.width * this.size.height;
+  }
+};
+
 console.log('decorators connected!');
 
 function DecLog(constructor: Function) {
@@ -40,10 +52,85 @@ class SuperComponent {
   get componentName() {
     return this.name;
   }
-
 }
 
+const newComp = new SuperComponent('I am super component');
+console.log('New component name: ', newComp.componentName);
+
+console.log('========================================================');
 // ====================================================
+
+// type syntax explanation
+type FnA = {
+  (s: string): object;
+  a: string;
+};
+// the same as 
+type FnA2 = (s: string) => object;
+
+type AnyClass = {
+  new(...args: any[]): object;
+};
+//the same as 
+type AnyClass2 = new(...args: any[]) => object;
+
+type overLoadedFn = {
+  (): void;
+  (s: string): number;
+}
+
+// overloaded fn what should be implemented 
+function FFF(a1: string): string;
+function FFF(a2: number): number;
+function FFF(a: string | number): string | number | undefined {
+  if (typeof a === 'string') {
+    return '';
+  }
+  if (typeof a === 'number') {
+    return 1;
+  }
+};
+
+type FF = {
+  (a1: string): string;
+  q: Symbol;
+  fn: {
+    (): void;
+    (s: string): number;
+    a: number;
+  }
+}
+
+
+function a(b: FF): {} {
+  const q = b("2")
+  const bq = b.q
+  return {};
+}
+
+a.q = 'qwe';
+
+class AA {}
+
+// const ac1: AnyClass = a;
+const ac2: AnyClass = AA;
+// const ac3: AnyClass = {};
+
+
+function Bind(_1: any, propName: string, descriptor: PropertyDescriptor): PropertyDescriptor {
+  const original = descriptor.value; // to get ref to the method
+  console.log('From Bind decorator - descriptor: ', descriptor);
+  const result = {
+    configurable: true,
+    enumerable: true,
+    get() {
+      console.log(`This from decorated method ${propName}: `, this);
+      return original.bind(this);
+    }
+  };
+  console.log('RESULT: ', result);
+  return result;
+}
 
 interface ComponentDecorator {
   selector: string;
@@ -51,7 +138,7 @@ interface ComponentDecorator {
 }
 
 function DecComponent(config: ComponentDecorator) {
-  return function<T extends { new(...args: any[]): object }>(Constructor: T) {
+  return function<T extends  new(...args: any[]) => object>(Constructor: T) {
     return class extends Constructor {
       constructor(...args: any[]) {
         super(...args);
@@ -60,17 +147,6 @@ function DecComponent(config: ComponentDecorator) {
       } 
     }
   }
-}
-
-function Bind(_1: any, _2: any, descriptor: PropertyDescriptor): PropertyDescriptor {
-  const original = descriptor.value; // to get the ref to the method
-  return {
-    configurable: true,
-    enumerable: true,
-    get() {
-      return original.bind(this);
-    }
-  };
 }
 
 @DecComponent({
@@ -93,9 +169,78 @@ class CardComponent {
   }
 }
 
+console.log('CLASS CARD COMPONENT DECORATED: ', CardComponent);
 const card = new CardComponent('Super Card');
+console.log('Instance of CARD COMPONENT DECORATED: ', card, card.logName);
 
 const btn = document.querySelector('#btn');
 btn?.addEventListener('click', card.logName ); // don't need to bind context because we applied the Bind decorator for the method logName 
 
+console.log('========================================================');
+
+
+// =================================================
+
+type ValidatorType = 'required' | 'email';
+
+interface ValidatorConfig {
+  [prop: string]: {
+    [validator: string]: ValidatorType;
+  }
+};
+
+const validators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+  validators[target.constructor.name] = {
+    ...validators[target.constructor.name],
+    [propName]: 'required'
+  }
+  console.log('Required decorator has been applied.');
+  console.log('Required decorator - target: ', target);
+  console.log('Required decorator - propName: ', propName);
+  console.log('Required decorator - validators: ', validators);
+}
+
+class Form {
+  @Required
+  public email: string | undefined;
+
+  constructor(email?: string) {
+    this.email = email;
+  }
+}
+
+function validate(obj: any): boolean {
+  console.log('validators from validate fn: ', validators);
+  const objConfig = validators[obj.constructor.name];
+  if (!objConfig) {
+    return true;
+  }
+  let isValid = true;
+  Object.keys(objConfig).forEach(key => {
+    if (objConfig[key] === 'required') {
+      isValid = isValid && !!obj[key];
+    }
+  })
+  return isValid;
+}
+
+const form1 = new Form();
+console.log('FORM: ', form1);
+
+if (validate(form1)) {
+  console.log('Valid: ', form1);
+} else {
+  console.log('Form1 Validation Error: ', form1);
+}
+
+const form2 = new Form('some@mail.com');
+console.log('FORM: ', form2);
+
+if (validate(form1)) {
+  console.log('Form2 Valid: ', form2);
+} else {
+  console.log('Validation Error');
+}
 
