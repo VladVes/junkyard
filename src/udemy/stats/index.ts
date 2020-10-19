741c202afba39acde66f5eb79bdd4c6ed7e42031
VladVes <veselov.workflow@gmail.com>
2020-10-19 21:17:48 +0700

    update

--- src/udemy/stats/index.ts
+++ src/udemy/stats/index.ts
@@ -1,13 +1,10 @@
 import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResult";
 
 const reader = new CsvFileReader("./src/udemy/stats/fixtures/football.csv");
 reader.read();
 
-enum MatchResult {
-  HomeWin = "H",
-  AwayWin = "A",
-  Draw = "D",
-}
+console.log(reader.data[0][0]);
 
 let manUnitedWins = 0;
 
