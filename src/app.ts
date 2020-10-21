71a7c1f4198d8191bc407a2c579bac4a96c882e8
VladVes <veselov.workflow@gmail.com>
2020-10-21 18:28:08 +0700

    rename

--- src/app.ts
+++
@@ -1,5 +0,0 @@
-const message: string = 'Hello TS!';
-console.log(message);
-
-// const btn = document.querySelector('#btn'); // no error because we have added "DOM" to the lib [] in the tsconfig.json
-// btn?.addEventListener('click', () => console.log('BTN HAS CLICKED!')) // btn? we need 
