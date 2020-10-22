44f94cf434deb21abb0312761aec4497ee0887eb
VladVes <veselov.workflow@gmail.com>
2020-10-22 22:26:58 +0700

    update

--- Web/src/index.ts
+++ Web/src/index.ts
@@ -1,1 +1,11 @@
-console.log("The Web Framework");
+import { User } from "./models/User";
+import axios from "axios";
+
+// const user = new User({ id: 1 });
+const user = new User({ name: "Bob", age: 14 });
+// user.set({ name: "Ivan", age: 35 });
+user.save();
+
+// setTimeout(() => {
+//   console.log(user);
+// }, 4000);
