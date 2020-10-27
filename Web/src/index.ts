def678442f4f6f9ddec8e1caace70d3cbbf98b4a
VladVes <veselov.workflow@gmail.com>
2020-10-27 19:32:05 +0700

    update

--- Web/src/index.ts
+++ Web/src/index.ts
@@ -2,8 +2,10 @@ import { User } from "./models/User";
 import axios from "axios";
 
 // const user = new User({ id: 1 });
-const user = new User({ name: "Jane", age: 20 });
+const user = new User({ id: 1, name: "ZZZ", age: 0 });
 
 console.log(user.get("name"));
-user.on("change", () => console.log("user has changed!"));
-user.trigger("change");
+user.on("change", () => console.log("changed", user));
+user.on("save", () => console.log("saved", user));
+
+user.save();
--- Web/src/models/User.ts
+++ Web/src/models/User.ts
@@ -1,6 +1,7 @@
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
 import { Attributes } from "./Attributes";
+import { AxiosPromise, AxiosResponse } from "axios";
 
 export interface UserProps {
   id?: number;
@@ -13,9 +14,9 @@ type Callback = () => void;
 const rootUrl = "http://localhost:3000/users";
 
 export class User {
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  public attributes: Attributes<UserProps>;
+  private events: Eventing = new Eventing();
+  private sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  private attributes: Attributes<UserProps>;
 
   constructor(attrs: UserProps) {
     this.attributes = new Attributes<UserProps>(attrs);
@@ -31,5 +32,31 @@ export class User {
 
   get get() {
     return this.attributes.get;
+  }
+
+  set(update: UserProps): void {
+    this.attributes.set(update);
+    this.events.trigger("change");
+  }
+
+  fetch(): void {
+    const id = this.attributes.get("id");
+    if (typeof id !== "number") {
+      throw new Error("Cannot fetch without an id");
+    }
+
+    this.sync.fetch(id).then((response: AxiosResponse) => {
+      this.set(response.data);
+    });
+  }
+
+  save(): void {
+    const attributes = this.attributes.getAll();
+    this.sync
+      .save(attributes)
+      .then((response: AxiosResponse): void => {
+        this.trigger("save");
+      })
+      .catch(() => this.trigger("error"));
   }
 }
