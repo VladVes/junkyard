6e718410fadbf8f4bc7aae90b12bdc2b0539e47f
VladVes <veselov.workflow@gmail.com>
2021-03-29 19:16:49 +0700

    update

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,10 +1,33 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-class App extends React.Component {
+interface AppProps {
+  color?: string;
+}
+
+// const App = (props: AppProps): JSX.Element => {
+//   return <div>Hi there! {props.color}</div>;
+// };
+
+class App extends React.Component<AppProps> {
+  state = { counter: 0 };
+
+  onIncrement = (): void => this.setState({ counter: this.state.counter + 1 });
+
+  onDecrement = (): void => this.setState({ counter: this.state.counter - 1 });
+
   render() {
-    return <div>Hi there!</div>;
+    return (
+      <div>
+        <div>Hi there! {this.props.color}</div>
+        <div>
+          <button onClick={this.onIncrement}>Increment</button>
+          <button onClick={this.onDecrement}>Decrement</button>
+          {this.state.counter}
+        </div>
+      </div>
+    );
   }
 }
 
-ReactDom.render(<App />, document.querySelector("#root"));
+ReactDom.render(<App color="red" />, document.querySelector("#root"));
